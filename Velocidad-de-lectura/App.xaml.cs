c6d77a0241785029703b525f911a0b9690d9a9a3
G. Mizael Mtz Hdz <g.mizael@hotmail.com>
2020-01-23 03:07:08 -0600

    Initial
    Initial

---
+++ Velocidad-de-lectura/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Velocidad_de_lectura.Services;
+using Velocidad_de_lectura.Views;
+
+namespace Velocidad_de_lectura
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Velocidad-de-lectura/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Velocidad_de_lectura.Models;
+
+namespace Velocidad_de_lectura.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
