0c6963e30b8df7d366f0d67beedac77b9c991e1e
G. Mizael Mtz Hdz <g.mizael@hotmail.com>
2020-02-02 03:53:44 -0600

    Avance en la creación de interfaz de categoría

---
+++ Velocidad-de-lectura/Models/CategoriaModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Velocidaddelectura.Models
+{
+    public class CategoriaModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Velocidad-de-lectura/ViewModels/CategoriaViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Velocidaddelectura.Models;
+using Xamarin.Forms;
+
+namespace Velocidaddelectura.ViewModels
+{
+    public class CategoriaViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<CategoriaModel> NombreCategoria { get; set; }
+        public INavigation Navigation { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CategoriaViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+            NombreCategoria = new ObservableCollection<CategoriaModel>();
+
+            NombreCategoria.Add(new CategoriaModel()
+            {
+                Id=1,
+                Nombre="Arte",
+                Description="Arte"
+            });
+            NombreCategoria.Add(new CategoriaModel()
+            {
+                Id = 2,
+                Nombre = "Ciencia",
+                Description = "Ciencia"
+            });
+            NombreCategoria.Add(new CategoriaModel()
+            {
+                Id = 3,
+                Nombre = "Matemáticas",
+                Description = "Matemáticas"
+            });
+        }
+    }
+}
