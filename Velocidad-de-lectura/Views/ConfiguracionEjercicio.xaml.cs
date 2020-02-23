c9de1fae11e22a84c731036cf7f97f281aabd043
G. Mizael Mtz Hdz <g.mizael@hotmail.com>
2020-02-23 02:23:16 -0600

    Configurar ejercicio actual

--- Velocidad-de-lectura/Views/ConfiguracionEjercicio.xaml.cs
+++ Velocidad-de-lectura/Views/ConfiguracionEjercicio.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Velocidaddelectura.Models;
+using Velocidaddelectura.Services;
 using Velocidaddelectura.ViewModels;
 using Xamarin.Forms;
 
@@ -13,14 +14,66 @@ namespace Velocidaddelectura.Views
         {
             InitializeComponent();
             this.ejercicio = ejercicio;
-            this.ejercicio.Segundos = 30;
+            this.ejercicio.Segundos = 0;
             this.ejercicio.Texto = (new ExtraccionContenidoViewModel()).GetContenido(this.ejercicio.Categoria);
             Application.Current.MainPage.DisplayAlert("Palabras", this.ejercicio.Texto, "Continuar");
 
         }
         private void BtnSeleccion_Clicked(object sender, EventArgs e)
         {
+            switch(PickerTiempo.SelectedIndex)
+            {
+                case 0:
+                    this.ejercicio.Segundos = 15;
+                    break;
+                case 1:
+                    this.ejercicio.Segundos = 30;
+                    break;
+                case 2:
+                    this.ejercicio.Segundos = 60;
+                    break;
+            }
+            switch (PickerLetra.SelectedIndex)
+            {
+                case 0:
+                    this.ejercicio.TamanoFuente = 14;
+                    break;
+                case 1:
+                    this.ejercicio.TamanoFuente = 24;
+                    break;
+                case 2:
+                    this.ejercicio.TamanoFuente = 28;
+                    break;
+                case 3:
+                        this.ejercicio.TamanoFuente = 34;
+                break;
+            }
             Navigation.PushAsync(new Velocidad(ejercicio), true);
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            LlenarEtiquetasTiempo();
+            LlenarEtiquetasTamanoLetra();
+        }
+        private void LlenarEtiquetasTiempo()
+        {
+            var etiquetas = new List<string>();
+            etiquetas.Add("15 Segundos");
+            etiquetas.Add("30 Segundos");
+            etiquetas.Add("60 Segundos");
+            PickerTiempo.ItemsSource = etiquetas;
+            PickerTiempo.SelectedIndex = 2;
+        }
+        private void LlenarEtiquetasTamanoLetra()
+        {
+            var etiquetas = new List<string>();
+            etiquetas.Add("Tamaño 14");
+            etiquetas.Add("Tamaño 24");
+            etiquetas.Add("Tamaño 28");
+            etiquetas.Add("Tamaño 34");
+            PickerLetra.ItemsSource = etiquetas;
+            PickerLetra.SelectedIndex = 2;
         }
     }
 }
