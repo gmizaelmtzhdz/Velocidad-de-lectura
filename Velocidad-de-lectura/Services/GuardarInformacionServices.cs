6b8e1256d081235d03cf4440ac0d9a90e9110b8d
G. Mizael Mtz Hdz <g.mizael@hotmail.com>
2020-02-24 01:50:15 -0600

    interfaz de historial de ejercicios hechos

---
+++ Velocidad-de-lectura/Services/GuardarInformacionServices.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Velocidaddelectura.Models;
+using Xamarin.Forms;
+
+namespace Velocidaddelectura.Services
+{
+    public class GuardarInformacionServices
+    {
+        public GuardarInformacionServices()
+        {
+
+        }
+        public async Task GuardarInformacion(Ejercicio ejercicio)
+        {
+            List<Ejercicio> historico = this.ObtenerInformacion();
+            historico.Add(ejercicio);
+
+            //Application.Current.MainPage.DisplayAlert("Json", JsonConvert.SerializeObject(historico), "Continuar");
+
+            Application.Current.Properties["historico_velocidad"] = JsonConvert.SerializeObject(historico);
+            await Application.Current.SavePropertiesAsync();
+        }
+        public List<Ejercicio> ObtenerInformacion()
+        {
+            List < Ejercicio > historico=new List<Ejercicio>();
+            if (Application.Current.Properties.ContainsKey("historico_velocidad"))
+            {
+                var json = Application.Current.Properties["historico_velocidad"].ToString();
+                try
+                {
+                    historico = JsonConvert.DeserializeObject<List<Ejercicio>>(json);
+                }catch(Exception e)
+                {
+                    historico = new List<Ejercicio>();
+                }
+            }
+            return historico;
+        }
+    }
+}
